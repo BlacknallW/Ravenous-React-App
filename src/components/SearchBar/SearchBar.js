e38b4d8f7d5a190d4ab1129a910c7088c9079014
William Blacknall <williamblacknall@gmail.com>
2019-11-03 22:06:01 -0500

    Added map function to search bar. Building it out bit by bit

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,3 +1,35 @@
 import React from 'react';
 import "./SearchBar.css";
 
+const sortByOptions = {
+    "Best Match": "best_mach",
+    "Highest Rated": "rating",
+    "Most Reviewed": "review_count"
+}
+
+export default function SearchBar(props){
+    renderSortByOptions = () =>{
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption]
+
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        });
+    }
+
+    return (
+        <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+            <ul>
+            <!-- Use .renderSortByOptions() to sort the businesses by their options -->
+            </ul>
+        </div>
+        <div className="SearchBar-fields">
+            <input placeholder="Search Businesses" />
+            <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+            <a>Let's Go</a>
+        </div>
+        </div>
+    )
+}
